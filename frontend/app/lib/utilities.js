7469546adbfd79137b2fe67e172079112d33af81
Kumar Abhirup <kumar@iqubex.com>
2019-10-18 12:42:28 +0530

    👌 IMPROVE: Allow empty arena background

--- frontend/app/lib/utilities.js
+++ frontend/app/lib/utilities.js
@@ -22,8 +22,9 @@ function drawBackground() {
   for (let i = -cells / 2; i < cells / 2; i++) {
     for (let j = -cells / 2; j < cells / 2; j++) {
       push()
-      if (imgArenaBackground)
+      if (imgArenaBackground) {
         image(imgArenaBackground, i * bgSize, j * bgSize, bgSize, bgSize)
+      }
       pop()
     }
   }
